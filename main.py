3c6ae7a8c75eebfba0da44a695a5c8be2e4db899
Valeriy Rybalkin <ntsdu.rbl@gmail.com>
2018-12-27 22:14:23 +0200

    Homework #1

---
+++ main.py
@@ -0,0 +1,24 @@
+#Homework Assignment #1
+
+#Metadata variables
+SongName = "Echoes"
+Album = "Rescue & Restore"
+Artist = "August Burns Red"
+DurationTimeSeconds = 268
+Gerne = "metalcore"
+YearReleased = 2013
+MonthReleased = "June"
+DayOfMonthReleased = "25"
+Label = "Solid State"
+
+print(SongName)
+print(Album)
+print(Artist)
+print(DurationTimeSeconds)
+print(Gerne)
+print(YearReleased)
+print(MonthReleased)
+print(DayOfMonthReleased)
+print(Label)
+
+
