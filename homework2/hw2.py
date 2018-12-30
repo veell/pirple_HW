e4f0e35982a5affba7cc5bf2a90606521a869190
Valeriy Rybalkin <ntsdu.rbl@gmail.com>
2018-12-30 15:34:09 +0200

    Add 2-nd homework

---
+++ homework2/hw2.py
@@ -0,0 +1,16 @@
+def Year():
+    return 2013
+
+def gerne():
+    return "metalcore"
+
+def artist():
+    return "August Burns Red"
+
+def year():
+    return 2013
+
+def isGroup():
+    return True
+
+print("Gerne:",gerne(),"\nartist:",artist(),"\nyear :",year(),"\nartist is group: ",isGroup())
