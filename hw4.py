6c908a5fe24aab9c3a66dbe3c0e76e267b6b569c
Valeriy Rybalkin <ntsdu.rbl@gmail.com>
2019-01-01 15:15:15 +0200

    Add advanced to homework 4

--- hw4.py
+++ hw4.py
@@ -2,14 +2,22 @@
 myUniqueList = []
 #Function that adds only unique elements to myUniqueList
 def addUniq(thing):
-    if myUniqueList.count(thing) == 0: #if occurences of parameter is 
+    if myUniqueList.count(thing) == 0: #if occurences of parameter is in List
         myUniqueList.append(thing)
         return True
     else:
         return False
+#Extra
+myLeftovers = []
+def addUniqWithRejects(thing):
+    if myUniqueList.count(thing) == 0: #if occurences of parameter is in List
+        myUniqueList.append(thing)
+        return True
+    else:
+        myLeftovers.append(thing)
+        return False
 
-
-#Testing this function
+#Testing addUniq function
 print(addUniq(1))
 print(myUniqueList)
 print(addUniq(7))
@@ -18,7 +26,30 @@ print(addUniq(1))
 print(myUniqueList)
 print(addUniq(78))
 print(myUniqueList)
-print(addUniq("hello"))
+print(addUniq("word"))
 print(myUniqueList)
-print(addUniq("hello"))
+print(addUniq("word"))
 print(myUniqueList)
+
+#Testing addUniqWithRejects function
+#Initializing myUniqueList with empty list
+print("test#2")
+myUniqueList = []
+print(addUniqWithRejects(1))
+print(myUniqueList)
+print(myLeftovers)
+print(addUniqWithRejects(7))
+print(myUniqueList)
+print(myLeftovers)
+print(addUniqWithRejects(1))
+print(myUniqueList)
+print(myLeftovers)
+print(addUniqWithRejects(78))
+print(myUniqueList)
+print(myLeftovers)
+print(addUniqWithRejects("word"))
+print(myUniqueList)
+print(myLeftovers)
+print(addUniqWithRejects("word"))
+print(myUniqueList)
+print(myLeftovers)
