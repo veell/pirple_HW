bc88e96b4331f918890032b16332ad5a0b0d186d
Valeriy Rybalkin <ntsdu.rbl@gmail.com>
2019-01-02 14:50:44 +0200

    Homework #5 with extra task

--- hw5.py
+++ hw5.py
@@ -2,8 +2,14 @@
 """
 Created on Tue Jan  1 22:18:28 2019
 
-@author: Admin
+@author: Valeriy Rybalkin
 """
+def isPrime(num):
+    for count in range(2,int(num/2)+1):#Number must in range must be integer
+    #There is no sense to get modulo to numbers greater then half of checked number
+        if num%count == 0:#
+            return False
+    return True #if loop entirely was checked without returning False
 
 for count in range(1,101):
     if (count%3 == 0) and (count%5 == 0):
@@ -14,5 +20,8 @@ for count in range(1,101):
         continue
     elif count%5 == 0:
         print("Buzz")
+        continue
+    if(isPrime(count)):
+        print("Prime")
         continue
     print(count)
